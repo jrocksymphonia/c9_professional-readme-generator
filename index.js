ea268ec2017499ea248b636d3a1db423146e3915
jrocksymphonia <122930110+jrocksymphonia@users.noreply.github.com>
2023-04-06 17:50:45 -0400

    made acceptance criteria easy to read

--- index.js
+++ index.js
@@ -9,16 +9,25 @@ const questions = [];
 // THEN a high-quality, professional README.md is generated 
 // with the title of my project and sections entitled 
 // Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+
 // WHEN I enter my project title
 // THEN this is displayed as the title of the README
+
 // WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
 // THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+
 // WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// THEN a badge for that license is added near the top of the README 
+// and a notice is added to the section of the README entitled License 
+// that explains which license the application is covered under
+
 // WHEN I enter my GitHub username
 // THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+
 // WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// THEN this is added to the section of the README entitled Questions, 
+// with instructions on how to reach me with additional questions
+
 // WHEN I click on the links in the Table of Contents
 // THEN I am taken to the corresponding section of the README
 
@@ -41,30 +50,45 @@ inquirer
     },
     {
       type: 'input',
-      name: 'food',
+      name: 'Installation',
       message: 'What is your favorite food?',
     },
     {
       type: 'input',
-      name: 'github',
+      name: 'Usage',
       message: 'Enter your GitHub Username',
     },
     {
       type: 'input',
-      name: 'linkedin',
+      name: 'License',
       message: 'Enter your LinkedIn URL.',
+    },
+    {
+        type: 'input',
+        name: 'Contributing',
+        message: 'Enter your LinkedIn URL.',
+    },
+    {
+        type: 'input',
+        name: 'Tests',
+        message: 'Enter your LinkedIn URL.',
+    },
+    {
+        type: 'input',
+        name: 'Questions',
+        message: 'Enter your LinkedIn URL.',
     },
   ])
   .then((answers) => {
-    const htmlPageContent = generateHTML(answers);
+    const htmlPageContent = generateREADME(answers);
 
-    fs.writeFile('index.html', htmlPageContent, (err) =>
+    fs.writeFile('readme.md', htmlPageContent, (err) =>
       err ? console.log(err) : console.log('Successfully created index.html!')
     );
   });
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const generateREADME = (fileName, data) =>
 
 // TODO: Create a function to initialize app
 function init() {}
